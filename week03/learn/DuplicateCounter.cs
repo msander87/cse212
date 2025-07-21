3a5d7bcc2ccc3eecdd02e7d1690d37b3ed7e6f5e
msander87 <msander1987@gmail.com>
2025-07-21 14:14:22 -0300

    week 3 learning

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -1,4 +1,7 @@
-﻿public class DuplicateCounter
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; 
+public class DuplicateCounter
 {
     //Count how many duplicates are in a collection of data.
 
@@ -24,7 +27,8 @@
 
     private static int CountDuplicates(int[] data)
     {
-        // Add code here.
-        return 0;
+        HashSet<int> dataSet = new HashSet<int>(data);
+        int duplicates = data.Length - dataSet.Count;
+        return duplicates;
     }
 }
